0fcd55ed4f0a8e5c4a99b2089a97301574be53b9
Kevin <kevin.dias.20160@gmail.com>
2022-01-14 19:38:14 -0300

    tipos dinamicos e fortes

---
+++ tipos_dinamicos_forte.py
@@ -0,0 +1,29 @@
+"""
+Conjunto de regras para definir como os elementos do programa se relacionam.
+"""
+
+"""
+Python é tipagem dinamica e forte.
+"""
+
+"""
+Python verifica qual objecto está relacionado a variavel letras antes de fazer 
+algo.
+"""
+letras = 'abc'
+
+"""
+Python não fica convertendo seus objetos implicitamente.
+"""
+
+print(1 + int('1'))
+
+
+
+print(str(1) + '1')
+
+"""
+Python suporta sobrecarga de operadores.
+"""
+
+
